09ceab0d6123b0f41cd2f27b37a9034a9f4469c3
Sven Stam <ask@54.247.64.25>
2013-09-05 13:55:38 +0000

    misc resources

--- cape-client/src/main/java/com/almende/cape/agent/intf/CapePersonalAgentIntf.java
+++ cape-client/src/main/java/com/almende/cape/agent/intf/CapePersonalAgentIntf.java
@@ -1,6 +1,7 @@
 package com.almende.cape.agent.intf;
 
 import com.almende.eve.agent.AgentInterface;
+import com.almende.eve.rpc.annotation.Name;
 
 public interface CapePersonalAgentIntf extends AgentInterface {
 	public String getName();
@@ -8,7 +9,10 @@ public interface CapePersonalAgentIntf extends AgentInterface {
 	public String getEmail();
 	public String getPincode();
 	String getBestPhoneAddress();
-	void setLastAvailCall(long timestamp);
+	void setLastAvailCall( @Name("timestamp") long timestamp);
 
 	public String getMessageAgentURL();
+
+	public void setResource(@Name("key") String key, @Name("value")  String value);
+	public String getResource(@Name("key") String key);
 }
